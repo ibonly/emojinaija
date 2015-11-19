95e4a52aea20300e768fe5df34c0c54afc36a4b7
andela-iadeniyi <ibonly01@gmail.com>
2015-11-19 12:38:29 +0100

    Database connection heroku test

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 require 'vendor/autoload.php';
 
 use Slim\Slim;
+use Ibonly\NaijaEmoji\Emoji;
 
 $app = new Slim();
+$get = new Emoji();
 
 $app->get('/', function () {
     echo "Hello";
 });
+echo $get->getAll();
 
 $app->run();
